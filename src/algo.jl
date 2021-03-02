d318fae8a056dc6cbce47c1ab7c6d5010aefbe39
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2021-03-02 12:22:35 -0300

    Add debug flags to forwards

--- src/algo.jl
+++ src/algo.jl
@@ -11,18 +11,23 @@ function choose(prob; norm=1.0)
   end
 end
 
-function forward(stages, state0)
+function forward(stages, state0; debug=0)
   for (i,stage) in enumerate(stages)
     set_initial_state!(stage, state0)
     JuMP.optimize!(stage)
 
     j = choose(stage.ext[:prob])
     state0 = JuMP.value.(stage.ext[:vars][1][:,j])
-    println("Going out from stage $i, branch $j, state $state0")
+    if debug > 0
+      println("Going out from stage $i, branch $j, state $state0")
+      if debug > 1
+        println("                         local decision: ", JuMP.value.(stage[:y]))
+      end
+    end
   end
 end
 
-function forward_dual(stages, state0)
+function forward_dual(stages, state0; debug=0)
   ϵ = 1e-1
 
   gamma0 = 1.0
@@ -33,10 +38,14 @@ function forward_dual(stages, state0)
     # Regularize probabilities, so that there's a chance to sample every scenario
     gammas = JuMP.value.(stage.ext[:vars][2]) .+ ϵ*gamma0
     gammas_r = gammas .+ ϵ*gamma0
-    println("  ", gammas)
+    if debug > 0
+      println("  ", gammas)
+    end
     j = choose(gammas_r, norm=sum(gammas_r))
     state0 = JuMP.value.(stage.ext[:vars][1][:,j])
     gamma0 = gammas[j]
-    println("Going out from stage $i, branch $j, state $state0, prob $gamma0")
+    if debug > 0
+      println("Going out from stage $i, branch $j, state $state0, prob $gamma0")
+    end
   end
 end
