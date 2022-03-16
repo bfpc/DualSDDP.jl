19f04c5f6840f5c63c7ae8055820b074bb8053f4
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2022-03-16 06:46:24 -0300

    Update 2d example
    Include \lambda (E-AVAR mixing)
    Default Lipschitz factor to 1
    Remove Simple primal, use only complete with upper bounds
    Remove GLPK directives (unused)

--- examples/2d_hydro/example_hist.jl
+++ examples/2d_hydro/example_hist.jl
@@ -4,39 +4,40 @@ Pkg.activate("../")
 import JuMP
 using DualSDDP
 
-lip_factor = 100
+###################
+# Parameters & Data
+
+lip_factor = 1
 
 include("hydro_hist.jl")
 beta = 0.4
+lambda = 0.7
 niters = [300, 300, 300]
 
 nstages = Hydro_Hist.nstages
 inivol  = Hydro_Hist.inivol
 
-risk      = mk_primal_avar(beta)
-risk_dual = mk_copersp_avar(beta)
+risk      = mk_primal_avar(beta; lambda=lambda)
+risk_dual = mk_copersp_avar(beta; lambda=lambda)
 
 import Gurobi
 env = Gurobi.Env()
 solver = JuMP.optimizer_with_attributes(() -> Gurobi.Optimizer(env), "OutputFlag" => 0)
-# import GLPK
-# solver = GLPK.Optimizer
-# 
 
+#####################
 # Solution algorithms
-# Pure primal
-primal_pb, primal_trajs, primal_lbs = primalsolve(Hydro_Hist.M, nstages, risk, solver, inivol, niters[1]; verbose=true)
 
 # Pure dual
-# Currently not working: with more scenarios does not show progress
 using Random: seed!
-seed!(1)
+seed!(3)
 dual_pb, dual_ubs = dualsolve(Hydro_Hist.M, nstages, risk_dual, solver, inivol, niters[2]; verbose=true)
 
 # Primal with interior bounds
-# Currently not working: sometimes the state escapes the convex hull
 seed!(2)
-_, _, _, primal_aux, Ubs = primalsolve(Hydro_Hist.M, nstages, risk, solver, inivol, niters[3]; verbose=true, ub=true)
+primal_pb, primal_trajs, primal_lbs, primal_aux, Ubs = primalsolve(Hydro_Hist.M, nstages, risk, solver, inivol, niters[3]; verbose=true, ub=true)
+
+###################
+# Graphs and bounds
 
 import PyPlot as plt
 plt.figure(figsize=(6,4));
