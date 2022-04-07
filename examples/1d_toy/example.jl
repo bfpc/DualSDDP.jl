3c9f7871b6c974bc4f9c9beb932fa92ca7d079e5
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2022-04-07 17:27:37 -0300

    Factor plot+save code from examples
    Still in "include()" from, but the general method should use Config
    and batch.jl

--- examples/1d_toy/example.jl
+++ examples/1d_toy/example.jl
@@ -33,37 +33,7 @@ dual_pb, dual_ubs = dualsolve(Hydro1d.M, nstages, risk_dual, solver, [inivol], n
 rec_ubs = primalub(Hydro1d.M, nstages, risk, solver, primal_trajs, ub_step:ub_step:niters; verbose=true)
 
 # Primal with outer and inner bounds
+seed!(1)
 io_pb, io_lbs, io_ubs = problem_child_solve(Hydro1d.M, nstages, risk, solver, [inivol], niters; verbose=true)
 
-function plot_step_ub(ub; kwargs...)
-  xs = repeat(first.(ub), 1, 2)' |> (x -> reshape(x, 1,8))
-  ys = repeat(last.(ub), 1, 2)' |> (x -> reshape(x, 1,8))
-  plt.plot(xs[2:end], ys[1:end-1]; kwargs...)
-end
-
-import PyPlot as plt
-plt.figure(figsize=(6,4));
-plt.plot(1:niters, dual_ubs, label="Dual Upper bounds");
-plot_step_ub(rec_ubs, label="Inner Upper bounds");
-plt.plot(1:niters, io_ubs, label="IO Upper bounds");
-plt.plot(1:niters, io_lbs, label="IO Lower bounds");
-plt.plot(1:niters, primal_lbs, label="Lower bounds");
-plt.xlabel("iteration #");
-plt.legend();
-plt.savefig("bounds.pdf");
-
-plt.yscale("log");
-plt.savefig("bounds_semilog.pdf");
-
-gap_ub = [(n, u/primal_lbs[n] - 1) for (n,u) in rec_ubs];
-plt.figure(figsize=(6,4));
-plt.semilogy(1:niters, dual_ubs./primal_lbs .- 1; label="Dual UB / Primal LB");
-plot_step_ub(gap_ub; label="Recursive Inner UB / Primal LB");
-plt.semilogy(1:niters, io_ubs./io_lbs .- 1; label="IO UB/LB");
-plt.legend();
-plt.xlabel("iteration #");
-plt.title("Relative gap");
-plt.savefig("gap.pdf");
-
-import NPZ
-NPZ.npzwrite("bounds.npz", lb=primal_lbs, ub=dual_ubs);
+include("../ex_plot_save.jl")
