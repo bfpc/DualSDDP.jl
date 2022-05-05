126933cff53b1f0134a89fc12f020429e9a381d8
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2022-05-05 18:23:22 -0300

    Update graph_epsilon
    * Add a "table" function with just @printf;
    * Transform read / plot script in separate functions;
    * Separate graphs for different alphas/betas used;
    * Disable interactive during subplots to accelerate;

--- examples/graph_epsilon.jl
+++ examples/graph_epsilon.jl
@@ -1,15 +1,15 @@
 import Pkg
 Pkg.activate(".")
 
-using Plots
+import PyPlot as plt
 using Config: parse!, ConfigManager, total_combinations, load
+using Printf: @printf
 
-plot_gap=1e-7
+# Currently unused
+# plot_gap=1e-7
 
 cfg_name = ARGS[1]
 cfg = ConfigManager(cfg_name, @__DIR__)
-
-N = total_combinations(cfg)
 
 # These are "unparametrizable" yet
 parse!(cfg, 1)
@@ -17,13 +17,11 @@ parse!(cfg, 1)
 dir = cfg["experiment"]["dir"]
 lip_factor = cfg["parameters"]["Lip"]
 
-bounds_dict = Dict()
-epsilons = Set()
+function read_abe(cfg::ConfigManager)
+  bounds_dict = Dict()
+  N = total_combinations(cfg)
 
-lb = 1e15
-
-for idx=1:N
-    println(idx)
+  for idx=1:N
     parse!(cfg, idx)
     data = load(cfg)
 
@@ -31,21 +29,77 @@ for idx=1:N
     beta  = cfg["risk-aversion"]["beta"]
     epsilon = cfg["parameters"]["epsilon"]
 
-    bounds_dict[epsilon] = (data["dual ub"])
-    lb = min(data["dual ub"][end],lb)
-    #lb = 1.883e8
-    push!(epsilons, epsilon)
-end
-lb = (1-plot_gap)*lb
-
-p = plot()
-for epsilon in epsilons
-    plot!(p,bounds_dict[epsilon].-lb, yaxis=:log, label=string(epsilon))
-end
-plot(p)
-
-for k in 0:6
-    epsilon = 1/10^k
-    println("ε=$epsilon yields final gap with best lb= ",bounds_dict[epsilon][end]-lb/(1-plot_gap))
+    bounds_dict[(alpha, beta, epsilon)] = (data["dual ub"])
+  end
+  return bounds_dict
 end
 
+function sets_in_dict(bounds_dict)
+  alphas = Set()
+  betas = Set()
+  epsilons = Set()
+
+  for (α, β, ϵ) in keys(bounds_dict)
+    push!(alphas, α)
+    push!(betas, β)
+    push!(epsilons, ϵ)
+  end
+  alphas = sort([alphas...])
+  betas = sort([betas...])
+  epsilons = sort([epsilons...])
+
+  return alphas, betas, epsilons
+end
+
+function table_bounds(bounds_dict)
+  alphas, betas, epsilons = sets_in_dict(bounds_dict)
+
+  nalphas = length(alphas)
+  nbetas = length(betas)
+  for i in 1:nalphas
+    alpha = alphas[i]
+    for j in 1:nbetas
+      beta = betas[j]
+      @printf("Experiment α=%6.3f β=%6.3f:\n", alpha, beta)
+      for epsilon in epsilons
+        @printf("    ε=%.2e yields final bound at %.17f\n", epsilon, bounds_dict[(alpha, beta, epsilon)][end])
+      end
+    end
+  end
+end
+
+# Small multiples on alpha/beta
+function smallmultiples(bounds_dict; log::Bool=false)
+  alphas, betas, epsilons = sets_in_dict(bounds_dict)
+
+  nalphas = length(alphas)
+  nbetas = length(betas)
+  interact = plt.isinteractive()
+  plt.ioff()
+  fig, axs = plt.subplots(ncols=nalphas, nrows=nbetas, figsize=(14,14))
+  for i in 1:nalphas
+    alpha = alphas[i]
+    for j in 1:nbetas
+      beta = betas[j]
+      lb = minimum([bounds_dict[(alpha, beta, e)][end] for e in epsilons])
+      for epsilon in epsilons
+        axs[i,j].plot(bounds_dict[(alpha, beta, epsilon)] .- lb, label=string(epsilon))
+      end
+      axs[i,j].set_title("α = $alpha, β = $beta")
+      if log
+        axs[i,j].yscale("log")
+      end
+    end
+  end
+  axs[1,end].legend(title="Regularization ϵ")
+  fig.suptitle("Upper bounds in dual algorithm - Lipschitz constant = $lip_factor")
+  fig.tight_layout()
+  plt.show()
+  if interact
+    plt.ion()
+  end
+end
+
+bounds_dict = read_abe(cfg)
+table_bounds(bounds_dict)
+smallmultiples(bounds_dict)
