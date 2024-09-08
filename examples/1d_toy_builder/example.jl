276d33eb548ea43c050ac49b3145443baf80af8d
Bernardo Freitas Paulo da Costa <bernardo.paulo@fgv.br>
2024-09-08 10:38:08 -0300

    Example using build.jl

---
+++ examples/1d_toy_builder/example.jl
@@ -0,0 +1,28 @@
+import Pkg
+Pkg.activate("../")
+
+using Random: seed!
+using DualSDDP
+
+lip_factor = 100
+
+include("hydro_conf.jl")
+include("hydro.jl")
+
+
+risk      = mk_primal_avar(alpha)
+risk_dual = mk_copersp_avar(alpha)
+
+
+# import Gurobi
+# env = Gurobi.Env()
+# solver = () -> Gurobi.Optimizer(env)
+import GLPK
+solver = GLPK.Optimizer
+
+M = Hydro1d.M
+state0 = Hydro1d.inivol
+
+include("../ex_calc_problems.jl")
+
+include("../ex_plot_save.jl")
---
+++ examples/1d_toy_builder/hydro.jl
@@ -0,0 +1,36 @@
+module Hydro1d
+
+include("hydro_scen.jl")
+
+# inivol
+inivol = [83.222]
+nscen = Main.nscen
+nstages = Main.nstages
+
+using DualSDDP: MSLBO
+using DualSDDP: build, SimpleLBO
+
+A = reshape([1.0, 0], 2, 1)
+B = reshape([-1.0, 0], 2, 1)
+T = [1.0 1 0 0 0; 1 0 1 1 1]
+
+c = [0., 1, 5, 10, 50]
+
+d = [[[0, 75.0]], [[i, 75.0] for i in inflows], [[i, 75.0] for i in inflows], [[i, 75.0] for i in inflows]]
+
+Ux = [100.0]
+Uy = [60.0, 200, 15, 15, 75]
+
+lb = fill(0.0, 4)
+ub = 75*50*[4.0, 3, 2, 1]
+Lip = 50*[4.0, 3, 2, 1]
+prob = [[1.0], ones(nscen)/nscen, ones(nscen)/nscen, ones(nscen)/nscen]
+
+stages = Vector{SimpleLBO}(undef, 4)
+for t in 1:4
+  stages[t] = SimpleLBO(A, B, T, c, d[t], lb[t], ub[t], Lip[t], prob[t])
+end
+
+M = build(stages, Ux, Uy)
+
+end
