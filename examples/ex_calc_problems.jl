ed4e25069e544741a1c4938ac384e3da1332104b
Bernardo Freitas Paulo da Costa <bernardo.paulo@fgv.br>
2024-11-11 18:22:01 -0300

    Update example plots to compare FW only to FW-BW algorithms

--- examples/ex_calc_problems.jl
+++ examples/ex_calc_problems.jl
@@ -5,9 +5,17 @@
 seed!(2)
 primal_pb, primal_trajs, primal_lbs, primal_times = primalsolve(M, nstages, risk, solver, state0, niters; verbose=true)
 
+# Pure primal with only forward pass
+seed!(2)
+primal_pb_fw, primal_trajs_fw, primal_lbs_fw, primal_times_fw = primalsolve(M, nstages, risk, solver, state0, niters; verbose=true, backward_solve=false)
+
 # Pure dual
 seed!(1)
 dual_pb, dual_ubs, dual_times = dualsolve(M, nstages, risk_dual, solver, state0, niters; verbose=true)
+
+# Pure dual with only forward pass
+seed!(1)
+dual_pb_fw, dual_ubs_fw, dual_times_fw = dualsolve(M, nstages, risk_dual, solver, state0, niters; verbose=true, backward_solve=false)
 
 # Recursive upper bounds over primal trajectories
 rec_ubs, rec_times = primalub(M, nstages, risk, solver, primal_trajs, ub_step:ub_step:niters; verbose=true)
@@ -15,4 +23,8 @@ rec_ubs, rec_times = primalub(M, nstages, risk, solver, primal_trajs, ub_step:ub
 # Primal with outer and inner bounds
 seed!(1)
 io_pb, io_lbs, io_ubs, io_times = problem_child_solve(M, nstages, risk, solver, state0, niters; verbose=true)
+
+# Primal with outer and inner bounds, only forward pass
+seed!(1)
+io_pb_fw, io_lbs_fw, io_ubs_fw, io_times_fw = problem_child_solve(M, nstages, risk, solver, state0, niters; verbose=true, backward_solve=false)
 
--- examples/ex_plot_save.jl
+++ examples/ex_plot_save.jl
@@ -12,11 +12,15 @@ function plot_step_ub(ub; kwargs...)
 end
 
 plt.figure(figsize=(6,4));
+plt.plot(1:niters, dual_ubs_fw, label="Dual Upper bounds, FW");
 plt.plot(1:niters, dual_ubs, label="Dual Upper bounds");
 plot_step_ub(rec_ubs, label="Inner Upper bounds");
+plt.plot(1:niters, io_ubs_fw, label="IO Upper bounds, FW");
 plt.plot(1:niters, io_ubs, label="IO Upper bounds");
 plt.plot(1:niters, io_lbs, label="IO Lower bounds");
+plt.plot(1:niters, io_lbs_fw, label="IO Lower bounds, FW");
 plt.plot(1:niters, primal_lbs, label="Lower bounds");
+plt.plot(1:niters, primal_lbs_fw, label="Lower bounds, FW");
 plt.xlabel("iteration #");
 plt.legend();
 plt.savefig("bounds.pdf");
@@ -26,8 +30,10 @@ plt.savefig("bounds_semilog.pdf");
 
 gap_ub = [(n, u/primal_lbs[n] - 1) for (n,u) in rec_ubs];
 plt.figure(figsize=(6,4));
+plt.semilogy(1:niters, dual_ubs_fw./primal_lbs_fw .- 1; label="Dual UB / Primal LB, FW");
 plt.semilogy(1:niters, dual_ubs./primal_lbs .- 1; label="Dual UB / Primal LB");
 plot_step_ub(gap_ub; label="Recursive Inner UB / Primal LB");
+plt.semilogy(1:niters, io_ubs_fw./io_lbs_fw .- 1; label="IO UB/LB, FW");
 plt.semilogy(1:niters, io_ubs./io_lbs .- 1; label="IO UB/LB");
 plt.legend();
 plt.xlabel("iteration #");
