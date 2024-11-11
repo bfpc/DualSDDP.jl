b725ab427c77e5158935564d9d3eb3b2580ddf6e
Bernardo Freitas Paulo da Costa <bernardo.paulo@fgv.br>
2024-11-11 13:58:14 -0300

    Update builder example to adapt to #stages

--- examples/1d_toy_builder/hydro.jl
+++ examples/1d_toy_builder/hydro.jl
@@ -16,7 +16,8 @@ T = [1.0 1 0 0 0; 1 0 1 1 1]
 
 c = [0., 1, 5, 10, 50]
 
-d = [[[0, 75.0]], [[i, 75.0] for i in inflows], [[i, 75.0] for i in inflows], [[i, 75.0] for i in inflows]]
+d = fill([[i, 75.0] for i in inflows], nstages)
+d[1] = [[0.0, 75.0]]
 
 Ux = [100.0]
 Uy = [60.0, 200, 15, 15, 75]
@@ -24,10 +25,11 @@ Uy = [60.0, 200, 15, 15, 75]
 lb = fill(0.0, 4)
 ub = 75*50*[4.0, 3, 2, 1]
 Lip = 50*[4.0, 3, 2, 1]
-prob = [[1.0], ones(nscen)/nscen, ones(nscen)/nscen, ones(nscen)/nscen]
+prob = fill(ones(nscen)/nscen, nstages)
+prob[1] = [1.0]
 
-stages = Vector{SimpleLBO}(undef, 4)
-for t in 1:4
+stages = Vector{SimpleLBO}(undef, nstages)
+for t in 1:nstages
   stages[t] = SimpleLBO(A, B, T, c, d[t], Ux, Uy, lb[t], ub[t], Lip[t], prob[t])
 end
 
